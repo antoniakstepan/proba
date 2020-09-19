68a80631fd5145a2c929c74570c87483b88a7509
antoniakstepan <antoniakstepan1@gmail.com>
2020-09-19 12:25:41 +0300

    Router in React

--- src/App.js
+++ src/App.js
@@ -1,54 +1,31 @@
 import React from 'react';
-// import logo from './logo.svg';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Dialogs from './Components/Dialogs/Dialogs';
 import Header from './Components/Header/Header';
+import Music from './Components/Music/Music';
 import Nav from './Components/Nav/Nav';
+import News from './Components/News/News';
 import Profile from './Components/Profile/Profile';
+import Settings from './Components/Settings/Settings';
 
 
 
 function App(props) {
   return (//
-    <div className="app-wrapper">
-      {/* <header className="header">
-           <img alt="My logo"src="https://www.w3schools.com/w3css/img_lights.jpg"/>
-         </header>  Замість цього буде компонента <Header/>*/}
-      <Header />
-
-
-      {/* <nav className="nav">
-           <div><a>Profile</a></div>
-           <div><a>Massages</a></div>
-           <div><a>News</a></div>
-           <div><a>Music</a></div>
-           <div><a>Settings</a></div>
-         </nav> Замість цього буде компонента <Nav/> */}
-      <Nav />
-      {/* <div className="content">
-           <div>
-           <img alt="Content"src="https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg"/>
-         </div>
-         <div>
-           Ava + discripion
-         </div>
-         <div>
-           My Posts
-           <div>
-             New Post
-           </div>
-           <div>Post 1</div>
-           <div>Post 2</div>
-         </div>
-         </div>Замість цього буде компонента <Profile/> */}
-
-      {/* <Profile /> */}
-      <div className="app-wrapper-content">
-     
-      <Dialogs />
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Nav />
+        <div className="app-wrapper-content">
+          <Route path="/profile"  component={Profile} />
+          <Route path="/dialogs"  component={Dialogs} />
+          <Route path="/News"  component={News}/>
+          <Route path="/Music" component={Music}/>
+          <Route path="/Settings" component={Settings}/>
+        </div>
       </div>
-      
-    </div>
+    </BrowserRouter>
   );
 
 }
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -6,11 +6,11 @@ import classes from './Nav.module.css'
 const Nav = () => {
     return (
         <nav className={classes.nav}>
-            <div className={classes.item}><a>Profile</a></div>
-            <div className={`${classes.item} ${classes.active}`}><a>Massages</a></div>
-            <div className={classes.item}><a>News</a></div>
-            <div className={classes.item}><a>Music</a></div>
-            <div className={classes.item}><a>Settings</a></div>
+            <div className={classes.item}><a href="/Profile">Profile</a></div>
+            <div className={`${classes.item} ${classes.active}`}><a href="/Dialogs">Massages</a></div>
+            <div className={classes.item}><a href="/News">News</a></div>
+            <div className={classes.item}><a href="/Music">Music</a></div>
+            <div className={classes.item}><a href="/Settings">Settings</a></div>
         </nav>
     )
 }
