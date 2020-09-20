c6e5197589ec3bea7480e13d27b783f326baa473
antoniakstepan <antoniakstepan1@gmail.com>
2020-09-20 13:37:20 +0300

    Dialogs Style1

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
-import classes from'./Dialogs.module.css';
+import classes from './Dialogs.module.css';
 
-const Dialogs= (props)=>{
-    return(
-        <div>
-            Dialogs
+const Dialogs = (props) => {
+    return (
+        <div className={classes.dialogs}>
+
+            <div className={classes.dialogsItems}>
+                <div className={classes.item + ' ' +classes.active}>
+                    Stepan
+               </div>
+                <div className={classes.item}>
+                    Sofia
+               </div>
+                <div className={classes.item}>
+                    Roksolana
+               </div>
+
+
+                <div className={classes.item}>
+                    Volodia
+               </div>
+
+
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>hi</div>
+                <div className={classes.message}>hu</div>
+                <div className={classes.message}>skjvb</div>
+                <div className={classes.message}>zxvn</div>
+                <div className={classes.message}>zxcvn</div>
+            </div>
         </div>
     )
 }
